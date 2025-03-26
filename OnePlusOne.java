689458ff4230722cf8edaefa5b59304de2f8605e
11florin <albuflorin929@yahoo.co.uk>
2025-03-26 22:02:05 +0000

    Add files via upload

---
+++ OnePlusOne.java
@@ -0,0 +1,14 @@
+class OnePlusOne
+{
+    public static void main(String[]args)
+    {
+        int i=12+4;
+        double d=4.0+4.0;
+        String s="HakerRank";
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s +" is the best place to learn and practice coding!");
+
+    }
+}
